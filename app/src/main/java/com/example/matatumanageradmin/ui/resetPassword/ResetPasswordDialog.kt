6e84cb700c94887f973e801c0f358ded14d1bb88
Emery Bashige <bemeryb@gmail.com>
2021-11-27 16:03:42 +0300

    can now reset password

---
+++ app/src/main/java/com/example/matatumanageradmin/ui/resetPassword/ResetPasswordDialog.kt
@@ -0,0 +1,45 @@
+package com.example.matatumanageradmin.ui.resetPassword
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.example.matatumanageradmin.databinding.ResetPasswordBinding
+import com.example.matatumanageradmin.utils.stringFromTl
+
+class ResetPassword : DialogFragment(){
+
+
+    lateinit var resetPasswordBinding: ResetPasswordBinding
+    internal lateinit var listener: ResetPasswordDialogListener
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+
+            val builder = AlertDialog.Builder(it)
+
+            val inflater = requireActivity().layoutInflater;
+            resetPasswordBinding = ResetPasswordBinding.inflate(inflater)
+            val view =resetPasswordBinding.root
+            builder.setView(view)
+
+
+            resetPasswordBinding.resetPasswordButton.setOnClickListener {
+                if(stringFromTl(resetPasswordBinding.emailResetPassword) == stringFromTl(resetPasswordBinding.emailResetPassword)){
+                    listener.onSaveButtonClicked(stringFromTl(resetPasswordBinding.emailResetPassword))
+                }
+            }
+
+            builder.create()
+
+        }?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    interface ResetPasswordDialogListener {
+        fun onSaveButtonClicked(email: String)
+    }
+
+    fun setListener(listener: ResetPasswordDialogListener) {
+        this.listener = listener
+    }
+}
